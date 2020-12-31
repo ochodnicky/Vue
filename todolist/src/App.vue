d3d7dd1c78736f0e0787a4145f1872d791bee5e4
Ondřej Ochodnický <ondrej.ochodnicky@avast.com>
2020-12-31 10:27:43 +0100

    format, respo fix;

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,41 +1,42 @@
 <template>
   <div class="wrapper">
+    <h1>Task List</h1>
 
-      <h1>Task List</h1>
-      
-      <form @submit.prevent="addItem"
-      class="todoAdd">
+    <form @submit.prevent="addItem" class="todoAdd">
       <div class="todoAdd__inputWrapper">
-        <input 
-        type="text"
-        placeholder="Add task..."
-        v-model="inputValue"
-        :class="[addInputClass, 'todoAdd__input']">
-        <div 
-        class="todoAdd__clear"
-        @click="clearInput">✕</div>
+        <input
+          type="text"
+          placeholder="Add task..."
+          v-model="inputValue"
+          class="todoAdd__input"
+        />
+        <div class="todoAdd__clear" @click="clearInput">✕</div>
       </div>
 
-        <button
-        type="button"
-        class="todoAdd__button"
-        @click="clearItems">Clear<br>All</button>
-      </form>
+      <button type="button" class="todoAdd__button" @click="clearItems">
+        Clear<br />All
+      </button>
+    </form>
 
-      <div class="todoList__nav">
-        <button 
+    <div class="todoList__nav">
+      <button
         type="button"
         @click="showCompleted(true)"
-        :class="[showCompletedItems ? 'isActive' : '']">All</button>
+        :class="[showCompletedItems ? 'isActive' : '']"
+      >
+        All
+      </button>
 
-        <button 
+      <button
         type="button"
         @click="showCompleted(false)"
-        :class="[!showCompletedItems ? 'isActive' : '']">To complete</button>
-      </div>
+        :class="[!showCompletedItems ? 'isActive' : '']"
+      >
+        To complete
+      </button>
+    </div>
 
-       <div class="tasks">
-      
+    <div class="tasks">
       <div class="message">
         <div class="message" v-if="myItems.length > 3">
           {{ messageFull }}
@@ -46,48 +47,51 @@
       </div>
 
       <ul class="todoList">
-        <li v-for="(item) in myItems"
-        :key="item.id"
-        :class="[item.completed ? 'isCompleted' : '', 'todoList__item']">
-        
+        <li
+          v-for="item in myItems"
+          :key="item.id"
+          :class="[item.completed ? 'isCompleted' : '', 'todoList__item']"
+        >
           <input
-          type="checkbox"
-          class="todoList__check"
-          :name="'todo-' + item.id"
-          :id="'todo-' + item.id">
+            type="checkbox"
+            class="todoList__check"
+            :name="'todo-' + item.id"
+            :id="'todo-' + item.id"
+          />
 
-          <label 
-          :for="'todo-' + item.id"
-          @click="completeItem(item.id)">
+          <label :for="'todo-' + item.id" @click="completeItem(item.id)">
           </label>
 
           <div class="todoList__inputWrapper">
-          <input 
-          type="text"
-          class="todoList__input"
-          :value="item.name"
-          :readonly="item.completed"
-          @keyup.enter="editItem($event, item.id)">
+            <input
+              type="text"
+              class="todoList__input"
+              :value="item.name"
+              :readonly="item.completed"
+              @keyup.enter="editItem($event, item.id)"
+            />
           </div>
 
           <button
-          type="button"
-          class="todoList__remove"
-          @click="removeItem(item.id)">✕</button>
+            type="button"
+            class="todoList__remove"
+            @click="removeItem(item.id)"
+          >
+            ✕
+          </button>
         </li>
       </ul>
-      </div>
+    </div>
 
     <div class="footer">
       <p>&copy; Ochodnický, Frýzlová</p>
     </div>
-
-    </div>
+  </div>
 </template>
 
 <!---------------------------->
 <script>
-import db from './db.js'
+import db from "./db.js";
 
 export default {
   data() {
@@ -97,90 +101,86 @@ export default {
       showCompletedItems: true,
       uniqueId: 0,
       messageFull: "' You have a lot to do... 🐝 '",
-      messageEmpty: "' The list is quite empty.. 😴 '"
+      messageEmpty: "' The list is quite empty.. 😴 '",
     };
   },
   firestore: {
-    items: db.collection('items'),
+    items: db.collection("items"),
   },
   computed: {
-    myItems () {
+    myItems() {
       let todoListItems;
 
       if (this.showCompletedItems) {
-          todoListItems = this.items;
+        todoListItems = this.items;
       } else {
-        todoListItems = this.items.filter(function(item) {
-            return item.completed == false;
+        todoListItems = this.items.filter(function (item) {
+          return item.completed == false;
         });
       }
-  
+
       return todoListItems;
     },
-    addInputClass () {
-      return this.inputValue.length > 1 ? 'green' : ''
-    }
+    addInputClass() {
+      return this.inputValue.length > 1 ? "green" : "";
+    },
   },
   methods: {
-    clearInput () {
+    clearInput() {
       this.inputValue = "";
       document.querySelector(".todoAdd__input").focus();
     },
-    addItem () {
+    addItem() {
       this.inputValue = this.inputValue.trim();
 
       if (this.inputValue.length > 1) {
-        db.collection("items")
-        .doc(`${this.uniqueId}`).set({
-            id: this.uniqueId,
-            name: this.inputValue,
-            completed: false
+        db.collection("items").doc(`${this.uniqueId}`).set({
+          id: this.uniqueId,
+          name: this.inputValue,
+          completed: false,
         });
-        this.inputValue="";
+        this.inputValue = "";
       }
     },
-    removeItem (id) {
-      db.collection('items')
-      .doc(`${id}`)
-      .delete()
+    removeItem(id) {
+      db.collection("items").doc(`${id}`).delete();
     },
-    editItem (event, id) {
-      db.collection("items")
-      .doc(`${id}`).update({
-          name: event.target.value
+    editItem(event, id) {
+      db.collection("items").doc(`${id}`).update({
+        name: event.target.value,
       });
     },
-    completeItem (id) {
+    completeItem(id) {
       let index = this.findIndex(id);
       let isCompleted = this.items[index].completed ? false : true;
-      
-      db.collection("items")
-      .doc(`${id}`).update({
-          completed: isCompleted
+
+      db.collection("items").doc(`${id}`).update({
+        completed: isCompleted,
       });
     },
-    showCompleted (value) {
+    showCompleted(value) {
       this.showCompletedItems = value;
     },
-    clearItems () {
-      if(confirm("Are you sure you want to clear all?")){
-      db.collection("items")
-      .get()
-      .then(res => {
-        res.forEach(element => {
-          element.ref.delete();
-        });
-      });
-    }},
+    clearItems() {
+      if (confirm("Are you sure you want to clear all?")) {
+        db.collection("items")
+          .get()
+          .then((res) => {
+            res.forEach((element) => {
+              element.ref.delete();
+            });
+          });
+      }
+    },
     findIndex(id) {
-      return this.items.findIndex(item => item.id === id);
-    }
+      return this.items.findIndex((item) => item.id === id);
+    },
   },
-  updated () {
-    if(this.items.length > 0) {
+  updated() {
+    if (this.items.length > 0) {
       let lastArray = this.items.length - 1;
       this.uniqueId = Number(this.items[lastArray].id) + 1;
     }
-  }
+  },
 };
 </script>
